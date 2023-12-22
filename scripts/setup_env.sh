c6788bc874f4733335de5f497bd44df748a724e4
cuonghx-gutech <cuonghx@gu-tech.com>
2023-12-22 10:28:57 +0700

    feat: add .env generate scripts

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -1,6 +1,6 @@
-if [ -z "${CSRF_TRUSTED_ORIGINS}" ]
+if [ -z "${CONFIG_DOMAIN}" ]
 then
-  echo CSRF_TRUSTED_ORIGINS env is missing
+  echo CONFIG_DOMAIN env is missing
   exit 1
 fi
 
@@ -29,14 +29,14 @@ then
 fi
 
 
-sed "s/SUPERUSER_EMAIL_VALUE/$SUPERUSER_EMAIL/g; s/SUPERUSER_USERNAME_VALUE/$SUPERUSER_USERNAME/g; s/SUPERUSER_PASSWORD_VALUE/$SUPERUSER_PASSWORD/g; s|CSRF_TRUSTED_ORIGINS_VALUE|$CSRF_TRUSTED_ORIGINS|g;" $1 ./container_env_files_templates/cfg.env > ./container_env_files/cfg.env
+sed "s/SUPERUSER_EMAIL_VALUE/$SUPERUSER_EMAIL/g; s/SUPERUSER_USERNAME_VALUE/$SUPERUSER_USERNAME/g; s/SUPERUSER_PASSWORD_VALUE/$SUPERUSER_PASSWORD/g; s|CONFIG_DOMAIN|$CONFIG_DOMAIN|g;" $1 ./container_env_files_templates/cfg.env > ./container_env_files/cfg.env
 
 sed "s/EXCHANGE_API_KEY_VALUE/$EXCHANGE_API_KEY/g" $1 ./container_env_files_templates/cgw.env > ./container_env_files/cgw.env
 
 sed "s/SUPERUSER_EMAIL_VALUE/$SUPERUSER_EMAIL/g; s/SUPERUSER_PASSWORD_VALUE/$SUPERUSER_PASSWORD/g" $1 ./container_env_files_templates/events.env > ./container_env_files/events.env
 
-sed "$1" ./container_env_files_templates/txs.env > ./container_env_files/txs.env
+sed "s|CONFIG_DOMAIN|$CONFIG_DOMAIN|g;" $1 ./container_env_files_templates/txs.env > ./container_env_files/txs.env
 
-sed "$1" ./container_env_files_templates/ui.env > ./container_env_files/ui.env
+sed "s|CONFIG_DOMAIN|$CONFIG_DOMAIN|g;" $1 ./container_env_files_templates/ui.env > ./container_env_files/ui.env
 
 sed "$1" ./container_env_files_templates/.env > .env
